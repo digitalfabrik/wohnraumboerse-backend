ec89063ee31e97c4449320c1cb7d010b4acd1318
Michael Markl <marklmichael98@gmail.com>
2018-02-26 23:09:05 +0100

    Add Offer model

--- src/api/offers.js
+++ src/api/offers.js
@@ -1,10 +1,18 @@
-import {Router} from 'express';
+import {Router} from 'express'
+import Offer from '../models/Offer'
 
 export default ({db}) => {
-    const router = new Router();
-    router.get('/', (req, res) => {
-        res.json(req.city)
-    });
+  const router = new Router()
+  router.get('/', (req, res) => {
+    res.json(req.city)
+  })
 
-    return router
+  router.put('/', (req, res) => {
+    const {email, formData} = req.body
+    const offer = new Offer({email, city: req.city, formData})
+    res.json(offer)
+
+  })
+
+  return router
 }
---
+++ src/models/Offer.js
@@ -0,0 +1,7 @@
+export default class Offer {
+  constructor ({email, city, formData}) {
+    this.email = email
+    this.city = city
+    this.formData = formData
+  }
+}
