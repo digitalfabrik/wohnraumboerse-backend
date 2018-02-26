053e1f478a86a615d1c8267cd365c3e1340270fa
Michael Markl <marklmichael98@gmail.com>
2018-02-26 22:39:30 +0100

    Config router first part

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,16 @@
-import { version } from '../../package.json';
-import { Router } from 'express';
+import {version} from '../../package.json';
+import {Router} from 'express';
 import facets from './facets';
 
-export default ({ config, db }) => {
-	let api = Router();
+export default () => {
+    const api = Router();
 
-	// mount the facets resource
-	api.use('/facets', facets({ config, db }));
 
-	// perhaps expose some API metadata at the root
-	api.get('/', (req, res) => {
-		res.json({ version });
-	});
+    api.param('city', (req, res, next, id) => {
+        req.city = id;
+        next()
+    });
+    api.use('/v0/:city', facets());
 
-	return api;
+    return api;
 }
