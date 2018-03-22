aa3c259148b1675dfc423a80dd95e976bf0f5757
Nikolas Klug <klug.nikolas@googlemail.com>
2018-03-22 08:42:52 +0100

    NEUBURG-2: Convert Offer to mongoose schema

--- src/database/Offer.js
+++ src/database/Offer.js
@@ -1,15 +1,49 @@
 import mongoose from 'mongoose'
+import forms from './forms'
 
 const offerSchema = mongoose.Schema({
-  id: String,
-  email: String,
-  city: String,
-  formData: Object,
-  expirationDate: Date,
-  confirmed: Boolean,
-  deleted: Boolean,
-  createdDate: Date,
-  hashedToken: String
+  email: {
+    type: String,
+    lowercase: true,
+    required: [true, 'ErrorMSG']
+  },
+  city: {
+    type: String,
+    lowercase: true,
+    required: [true, 'ErrorMSG']
+  },
+  expirationDate: {
+    type: Date,
+    required: [true, 'ErrorMSG']
+  },
+  confirmed: {
+    type: Boolean,
+    required: [true, 'ErrorMSG'],
+    default: false
+  },
+  deleted: {
+    type: Boolean,
+    required: [true, 'ErrorMSG'],
+    default: false
+  },
+  createdDate: {
+    type: Date,
+    required: [true, 'ErrorMSG'],
+    default: Date.now
+  },
+  hashedToken: {
+    type: String,
+    required: [true, 'ErrorMSG']
+  },
+  formData: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: Array.from(Object.keys(forms)), // Must be one of the forms defined in forms.js
+    required: [true, 'ErrorMSG']
+  }
 })
+
+offerSchema.query.actives = function (callback) {
+  return this.find().where('expirationDate').gt(Date.now()).exec(callback)
+}
 
 export default mongoose.model('Offer', offerSchema)
