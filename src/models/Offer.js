249dbf423ecb5a440a8cf78db13308e670fc2059
Nikolas Klug <klug.nikolas@googlemail.com>
2018-03-22 17:13:38 +0100

    NEUBURG-2: Add error messages to offer schema

--- src/models/Offer.js
+++ src/models/Offer.js
@@ -5,41 +5,41 @@ const offerSchema = mongoose.Schema({
   email: {
     type: String,
     lowercase: true,
-    required: [true, 'ErrorMSG']
+    required: [true, 'Missing email']
   },
   city: {
     type: String,
     lowercase: true,
-    required: [true, 'ErrorMSG']
+    required: [true, 'Missing city']
   },
   expirationDate: {
     type: Date,
-    required: [true, 'ErrorMSG']
+    required: [true, 'Missing expiration date']
   },
   confirmed: {
     type: Boolean,
-    required: [true, 'ErrorMSG'],
+    required: [true, 'Missing confirmed property'],
     default: false
   },
   deleted: {
     type: Boolean,
-    required: [true, 'ErrorMSG'],
+    required: [true, 'Missing deleted property'],
     default: false
   },
   createdDate: {
     type: Date,
-    required: [true, 'ErrorMSG'],
+    required: [true, 'Missing created date'],
     default: Date.now
   },
   hashedToken: {
     type: String,
-    required: [true, 'ErrorMSG'],
+    required: [true, 'Missing hashed token'],
     unique: true // Create a mongodb unique index
   },
   formData: {
     type: mongoose.Schema.Types.ObjectId,
     ref: Array.from(Object.keys(forms)), // Must be one of the forms defined in forms.js
-    required: [true, 'ErrorMSG']
+    required: [true, 'Missing form data']
   }
 })
 
