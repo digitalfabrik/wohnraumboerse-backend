78b9776d87ae51b90fc12c56c7d9903d9f2d0f7d
Nikolas Klug <klug.nikolas@googlemail.com>
2018-03-22 17:04:57 +0100

    Change database name

--- src/db.js
+++ src/db.js
@@ -1,8 +1,7 @@
 import mongoose from 'mongoose'
 
 export default callback => {
-  // connect to a database if needed, then pass it to `callback`:
-  mongoose.connect('mongodb://localhost/test')
+  mongoose.connect('mongodb://localhost/livingDB')
   const db = mongoose.connection
   callback(db)
 }
