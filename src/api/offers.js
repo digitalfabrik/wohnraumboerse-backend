0985526cc58aad46efe97d1147719bf48353febb
Nikolas Klug <klug.nikolas@googlemail.com>
2018-03-28 09:21:00 +0200

    Rename variables

--- src/api/offers.js
+++ src/api/offers.js
@@ -5,8 +5,8 @@ import {TOKEN_LENGTH} from '../utils/createToken'
 import HttpStatus from 'http-status-codes'
 import Offer from '../models/Offer'
 
-const validateMiddleware = (req, res, next) => {
-  const errors = validationResult(req)
+const validateMiddleware = (request, res, next) => {
+  const errors = validationResult(request)
   if (!errors.isEmpty()) {
     return res.status(HttpStatus.UNPROCESSABLE_ENTITY).json({errors: errors.mapped()})
   } else {
@@ -17,21 +17,21 @@ const validateMiddleware = (req, res, next) => {
 export default ({offerService}) => {
   const router = new Router()
 
-  router.get('/getAll', async (req, res) => {
+  router.get('/getAll', async (request, result) => {
     try {
       const queryResult = await offerService.getAllOffers()
-      return res.json(queryResult)
+      return result.json(queryResult)
     } catch (e) {
-      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e)
+      return result.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e)
     }
   })
 
-  router.get('/', async (req, res) => {
+  router.get('/', async (request, result) => {
     try {
-      const queryResult = await offerService.getActiveOffers(req.city)
-      return res.json(queryResult)
+      const queryResult = await offerService.getActiveOffers(request.city)
+      return result.json(queryResult)
     } catch (e) {
-      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e)
+      return result.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e)
     }
   })
 
@@ -40,14 +40,14 @@ export default ({offerService}) => {
     body('duration').isInt().toInt().custom(value => [3, 7, 14, 30].includes(value)),
     body('formData').exists(),
     validateMiddleware,
-    async (req, res) => {
-      const {email, formData, duration} = matchedData(req)
+    async (request, result) => {
+      const {email, formData, duration} = matchedData(request)
       try {
-        const token = await offerService.createOffer(req.city, email, formData, duration)
-        return res.status(HttpStatus.CREATED).json(token)
+        const token = await offerService.createOffer(request.city, email, formData, duration)
+        return result.status(HttpStatus.CREATED).json(token)
       } catch (e) {
         console.error(e)
-        return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e)
+        return result.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e)
       }
     }
   ])
@@ -55,22 +55,22 @@ export default ({offerService}) => {
   router.post(`/:token/confirm`, [
     param('token').isHexadecimal().isLength(TOKEN_LENGTH),
     validateMiddleware,
-    async (req, res) => {
+    async (request, result) => {
       try {
-        const {token} = matchedData(req)
+        const {token} = matchedData(request)
         const offer = new Offer(await offerService.getOfferByToken(token))
 
         if (!offer) {
-          return res.status(HttpStatus.NOT_FOUND).json('No such offer')
+          return result.status(HttpStatus.NOT_FOUND).json('No such offer')
         } else if (offer.isExpired() || offer.deleted) {
-          return res.status(HttpStatus.GONE).json('Offer not available')
+          return result.status(HttpStatus.GONE).json('Offer not available')
         }
 
         await offerService.confirmOffer(offer, token)
-        return res.status(HttpStatus.OK).end()
+        return result.status(HttpStatus.OK).end()
       } catch (e) {
         console.error(e)
-        return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e)
+        return result.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e)
       }
     }
   ])
@@ -79,22 +79,22 @@ export default ({offerService}) => {
     param('token').isHexadecimal().isLength(TOKEN_LENGTH),
     body('duration').isInt().toInt().custom(value => [3, 7, 14, 30].includes(value)),
     validateMiddleware,
-    async (req, res) => {
+    async (request, result) => {
       try {
-        const {token, duration} = matchedData(req)
+        const {token, duration} = matchedData(request)
         const offer = new Offer(await offerService.getOfferByToken(token))
 
         if (!offer) {
-          return res.status(HttpStatus.NOT_FOUND).json('No such offer')
+          return result.status(HttpStatus.NOT_FOUND).json('No such offer')
         } else if (offer.deleted || !offer.confirmed) {
-          return res.status(HttpStatus.BAD_REQUEST).json('Offer not available')
+          return result.status(HttpStatus.BAD_REQUEST).json('Offer not available')
         }
 
         await offerService.extendOffer(offer, duration, token)
-        return res.status(HttpStatus.OK).end()
+        return result.status(HttpStatus.OK).end()
       } catch (e) {
         console.error(e)
-        return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e)
+        return result.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e)
       }
     }
   ])
@@ -102,22 +102,22 @@ export default ({offerService}) => {
   router.delete(`/:token`, [
     param('token').isHexadecimal().isLength(TOKEN_LENGTH),
     validateMiddleware,
-    async (req, res) => {
+    async (request, result) => {
       try {
-        const {token} = matchedData(req)
+        const {token} = matchedData(request)
         const offer = new Offer(await offerService.getOfferByToken(token))
 
         if (!offer) {
-          return res.status(HttpStatus.NOT_FOUND).json('No such offer')
+          return result.status(HttpStatus.NOT_FOUND).json('No such offer')
         } else if (offer.deleted) {
-          return res.status(HttpStatus.BAD_REQUEST).json('Already deleted')
+          return result.status(HttpStatus.BAD_requestUEST).json('Already deleted')
         }
 
         await offerService.deleteOffer(offer)
-        return res.status(HttpStatus.OK).end()
+        return result.status(HttpStatus.OK).end()
       } catch (e) {
         console.error(e)
-        res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e)
+        result.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e)
       }
     }
   ])
