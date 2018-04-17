4ac721d6db1cbf770f1acd34916dc498114d9f69
Nikolas Klug <klug.nikolas@googlemail.com>
2018-04-17 20:06:50 +0200

    NEUBURG-18: Fix eslints in OfferService.js

--- src/services/OfferService.js
+++ src/services/OfferService.js
@@ -9,7 +9,7 @@ import forms from '../models/forms'
 const MILLISECONDS_IN_A_DAY = 1000 * 60 * 60 * 24
 
 export default class OfferService {
-  async createOffer (city: string, email: string, formData: mixed, duration: number) {
+  async createOffer (city: string, email: string, formData: mixed, duration: number): Promise<string> {
     const token = createToken()
 
     const form = new forms[city](formData)
@@ -30,14 +30,14 @@ export default class OfferService {
     return token
   }
 
-  getAllOffers () {
+  getAllOffers (): Promise<void> {
     return Offer.find()
       .select('-_id -__v')
       .populate({path: 'formData', select: '-_id -__v'})
       .exec()
   }
 
-  getActiveOffers (city: string) {
+  getActiveOffers (city: string): Promise<void> {
     return Offer.find()
       .select('-_id -__v')
       .where('city').equals(city)
@@ -47,7 +47,7 @@ export default class OfferService {
       .exec()
   }
 
-  async getOfferByToken (token: string) {
+  async getOfferByToken (token: string): Offer {
     // Don't populate, otherwise an 'Offer' Object cannot be properly created
     const offerResult = await Offer.findOne()
       .where('hashedToken').equals(hash(token))
@@ -55,28 +55,26 @@ export default class OfferService {
     return new Offer(offerResult)
   }
 
-  async confirmOffer (offer: Offer, token: string) {
-    if (offer.confirmed === true) {
-      return
+  async confirmOffer (offer: Offer, token: string): Promise<void> {
+    if (offer.confirmed !== true) {
+      const mailService = new MailService()
+      await mailService.sendConfirmationMail(offer, token)
+      await Offer.findByIdAndUpdate(offer._id, {confirmed: true}).exec()
     }
-    const mailService = new MailService()
-    await mailService.sendConfirmationMail(offer, token)
-    await Offer.findByIdAndUpdate(offer._id, {confirmed: true}).exec()
   }
 
-  async extendOffer (offer: Offer, duration: number, token: string) {
+  async extendOffer (offer: Offer, duration: number, token: string): Promise<void> {
     const newExpirationDate = new Date(Date.now() + duration * MILLISECONDS_IN_A_DAY).toISOString()
     const mailService = new MailService()
     await mailService.sendExtensionMail(offer, token)
     await Offer.findByIdAndUpdate(offer._id, {expirationDate: newExpirationDate}).exec()
   }
 
-  async deleteOffer (offer: Offer) {
-    if (offer.deleted) {
-      return
+  async deleteOffer (offer: Offer): Promise<void> {
+    if (!offer.deleted) {
+      const mailService = new MailService()
+      await mailService.sendDeletionMail(offer)
+      await Offer.findByIdAndUpdate(offer._id, {deleted: true}).exec()
     }
-    const mailService = new MailService()
-    await mailService.sendDeletionMail(offer)
-    await Offer.findByIdAndUpdate(offer._id, {deleted: true}).exec()
   }
 }
