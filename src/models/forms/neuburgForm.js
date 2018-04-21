e0bba953e70fe4a5dac5b58b91db1f1101fef56e
Nikolas Klug <klug.nikolas@googlemail.com>
2018-04-21 21:42:12 +0200

    NEUBURG-19: Add first idea for the offer

--- src/models/forms/neuburgForm.js
+++ src/models/forms/neuburgForm.js
@@ -1,5 +1,75 @@
 // @flow
 
 export default {
-  temp: Number
+  surname: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  street: {
+    type: String,
+    required: true
+  },
+  plz: {
+    type: Number,
+    required: true,
+    min: 10000,
+    max: 99999
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  telephone: {
+    type: Number
+  },
+
+  totalArea: {
+    type: Number,
+    required: true
+  },
+  totalRooms: {
+    type: Number,
+    required: true
+  },
+  ofRooms: {
+    type: [String],
+    enum: ['bathroom', 'bedroom'],
+    validate: {
+      validator: function (values: Array<string>): boolean {
+        return values.length > 0
+      },
+      message: 'ofRooms can not be empty'
+    }
+  },
+
+  baseRent: {
+    type: Number,
+    required: true
+  },
+  runningCosts: {
+    type: Number,
+    required: true
+  },
+  ofRunningServices: {
+    type: [String]
+  },
+  hotWaterInHeatingCosts: {
+    type: Boolean,
+    default: false
+  },
+  additionalCosts: {
+    type: Number,
+    default: 0
+  },
+  ofAdditionalServices: {
+    type: [String]
+  },
+  agreedToDataProtection: {
+    type: Boolean,
+    required: true
+  }
 }
