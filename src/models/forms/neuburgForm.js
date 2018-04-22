d0b5c8af9726fe428d032e410048faf1ea1f1041
Nikolas Klug <klug.nikolas@googlemail.com>
2018-04-22 17:10:43 +0200

    NEUBURG-19: Add validation to the neuburg form

--- src/models/forms/neuburgForm.js
+++ src/models/forms/neuburgForm.js
@@ -1,32 +1,30 @@
 // @flow
 
 export default {
+  // landlord's data
   surname: {
     type: String,
     required: true
   },
   firstName: {
-    type: String,
-    required: true
+    type: String
   },
   street: {
-    type: String,
-    required: true
+    type: String
   },
   plz: {
     type: Number,
-    required: true,
     min: 10000,
     max: 99999
   },
   location: {
-    type: String,
-    required: true
+    type: String
   },
   telephone: {
-    type: Number
+    type: String
   },
 
+  // data on the object
   totalArea: {
     type: Number,
     required: true
@@ -37,7 +35,9 @@ export default {
   },
   ofRooms: {
     type: [String],
-    enum: ['bathroom', 'bedroom'],
+    lowercase: true,
+    enum: ['kitchen', 'bath', 'wc', 'child1', 'child2',
+      'child3', 'bed', 'hallway', 'store', 'basement', 'balcony'],
     validate: {
       validator: function (values: Array<string>): boolean {
         return values.length > 0
@@ -48,14 +48,23 @@ export default {
 
   baseRent: {
     type: Number,
+    min: 0,
     required: true
   },
   runningCosts: {
     type: Number,
-    required: true
+    min: 0,
+    default: 0
   },
   ofRunningServices: {
-    type: [String]
+    type: [String],
+    enum: ['heating', 'water', 'garbage', 'chimney', 'other'],
+    validate: {
+      validator: function (values: Array<string>): boolean {
+        return this.runningCosts === 0 || values.length > 0
+      },
+      message: 'ofRunningServices can not be empty if runningCosts > 0'
+    }
   },
   hotWaterInHeatingCosts: {
     type: Boolean,
@@ -63,13 +72,17 @@ export default {
   },
   additionalCosts: {
     type: Number,
+    min: 0,
     default: 0
   },
   ofAdditionalServices: {
-    type: [String]
-  },
-  agreedToDataProtection: {
-    type: Boolean,
-    required: true
+    type: [String],
+    enum: ['garage', 'other'],
+    validate: {
+      validator: function (values: Array<string>): boolean {
+        return this.additionalCosts === 0 || values.length > 0
+      },
+      message: 'ofAdditionalServices can not be empty if additionalCosts > 0'
+    }
   }
 }
