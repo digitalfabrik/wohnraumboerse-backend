d3e345818c997ce583658612321f126443a97dd7
Nikolas Klug <klug.nikolas@googlemail.com>
2018-05-19 14:27:44 +0200

    NEUBURG-21: Fix flow and linting

--- src/cities/CityConfig.js
+++ src/cities/CityConfig.js
@@ -1,14 +1,13 @@
 // @flow
 
 class CityConfig {
-
   cmsName: string
   hostName: string
   formsEnabled: boolean
   title: string
-  logo: string
+  logo: ?string
 
-  constructor (cmsName: string, hostname: string, formsEnabled: boolean, title: string, logo: string) {
+  constructor (cmsName: string, hostname: string, formsEnabled: boolean, title: string, logo: ?string) {
     this.cmsName = cmsName
     this.hostName = hostname
     this.formsEnabled = formsEnabled
