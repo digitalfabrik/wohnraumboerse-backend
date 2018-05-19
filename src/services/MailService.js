cfc9cb781c8a7a1687b5e61e14fdca249aa47176
Steffen Kleinle <klinzo@yahoo.de>
2018-05-19 11:57:23 +0200

    NEUBURG-13: Use moment.js to format dates

--- src/services/MailService.js
+++ src/services/MailService.js
@@ -4,6 +4,7 @@ import {createTransport, SMTPTransport} from 'nodemailer'
 import {compileFile} from 'pug'
 import Offer from '../models/Offer'
 import neuburgConfig from '../cityConfigs/neuburg'
+import moment from 'moment'
 
 const develop = process.env.NODE_ENV === 'development'
 
@@ -15,6 +16,8 @@ const renderExtensionMail = compileFile('src/views/extensionMail.pug')
 const getConfirmationUrl = (city: string, token: string): string => `http://neuburg.wohnen.integreat-app.de/offer/${token}/confirm`
 const getDeletionUrl = (city: string, token: string): string => `http://neuburg.wohnen.integreat-app.de/offer/${token}`
 const getExtensionUrl = (city: string, token: string): string => `http://neuburg.wohnen.integreat-app.de/offer/${token}/extend`
+
+const getFormattedDate = (date: Date): string => moment(date).locale('de').format('dddd, Do MMMM YYYY')
 
 export default class MailService {
   smtpConfig: SMTPTransport
@@ -28,12 +31,11 @@ export default class MailService {
   }
 
   async sendRequestConfirmationMail (offer: Offer, token: string): Promise<void> {
-    const subject = 'Bestätigung Ihres Wohnungsangebotes erforderlich'
+    const subject = 'Bestätigen Sie Ihr Wohnungsangebot'
     const confirmationUrl = getConfirmationUrl(offer.city, token)
     const {accentColor, logoSrc, portalName} = neuburgConfig
-    const html = renderRequestConfirmationMail({
-      subject, confirmationUrl, accentColor, logoSrc, portalName
-    })
+
+    const html = renderRequestConfirmationMail({subject, confirmationUrl, accentColor, logoSrc, portalName})
     if (!develop) {
       await this.sendMail({to: offer.email, subject, html})
     }
@@ -41,10 +43,11 @@ export default class MailService {
 
   async sendConfirmationMail (offer: Offer, token: string): Promise<void> {
     const subject = 'Informationen zu Ihrem Wohnungsangebot'
-    const expirationDate = new Date(offer.expirationDate).toDateString()
+    const expirationDate = getFormattedDate(offer.expirationDate)
     const deletionUrl = getDeletionUrl(offer.city, token)
     const extensionUrl = getExtensionUrl(offer.city, token)
     const {accentColor, logoSrc, portalName} = neuburgConfig
+
     const html = renderConfirmationMail({expirationDate, deletionUrl, extensionUrl, accentColor, logoSrc, portalName})
     if (!develop) {
       await this.sendMail({to: offer.email, subject, html})
@@ -54,6 +57,7 @@ export default class MailService {
   async sendDeletionMail (offer: Offer): Promise<void> {
     const subject = 'Löschung Ihres Wohnungsangebotes erfolgreich'
     const {accentColor, logoSrc, portalName} = neuburgConfig
+
     const html = renderDeletionMail({accentColor, logoSrc, portalName})
     if (!develop) {
       await this.sendMail({to: offer.email, subject, html})
@@ -62,13 +66,12 @@ export default class MailService {
 
   async sendExtensionMail (offer: Offer, token: string): Promise<void> {
     const subject = 'Verlängerung Ihres Wohungsangebotes erfolgreich'
-    const expirationDate = new Date(offer.expirationDate).toDateString()
+    const expirationDate = getFormattedDate(offer.expirationDate)
     const deletionUrl = getDeletionUrl(offer.city, token)
     const extensionUrl = getExtensionUrl(offer.city, token)
     const {accentColor, logoSrc, portalName} = neuburgConfig
-    const html = renderExtensionMail({
-      expirationDate, deletionUrl, extensionUrl, accentColor, logoSrc, portalName
-    })
+
+    const html = renderExtensionMail({expirationDate, deletionUrl, extensionUrl, accentColor, logoSrc, portalName})
     if (!develop) {
       await this.sendMail({to: offer.email, subject, html})
     }
