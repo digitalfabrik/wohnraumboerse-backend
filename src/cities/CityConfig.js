61beac3e78fcac834202ae11416113319786eb3b
Steffen Kleinle <klinzo@yahoo.de>
2018-05-19 14:10:52 +0200

    NEUBURG-13: Remove impressumUrl

--- src/cities/CityConfig.js
+++ src/cities/CityConfig.js
@@ -5,17 +5,15 @@ class CityConfig {
   hostName: string
   formsEnabled: boolean
   title: string
-  logo: string
-  impressumUrl: string
+  logo: ?string
 
-  constructor ({cmsName, hostname, formsEnabled, title, logo, impressumUrl}: {|cmsName: string,
-    hostname: string, formsEnabled: boolean, title: string, logo: string, impressumUrl: string|}) {
+  constructor ({cmsName, hostname, formsEnabled, title, logo}: {|cmsName: string,
+    hostname: string, formsEnabled: boolean, title: string, logo: ?string|}) {
     this.cmsName = cmsName
     this.hostName = hostname
     this.formsEnabled = formsEnabled
     this.title = title
     this.logo = logo
-    this.impressumUrl = impressumUrl
   }
 }
 
