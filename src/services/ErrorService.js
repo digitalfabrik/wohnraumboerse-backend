4f1465a9b3f07c567fda2894991d1e7aac00674c
Nikolas Klug <klug.nikolas@googlemail.com>
2018-06-29 18:11:21 +0200

    NEUBURG-10: Move error strings to object

--- src/services/ErrorService.js
+++ src/services/ErrorService.js
@@ -5,39 +5,46 @@ import _ from 'lodash'
 
 const develop = process.env.NODE_ENV === 'development'
 
+const errorTypes = {
+  token: 'token',
+  confirmation: 'confirmation',
+  validation: 'validation',
+  server: 'server'
+}
+
 export default class ErrorService {
   createInternalServerErrorResponse (error: Error): Error | ErrorResponse {
     console.error(error)
     if (develop) {
       return error
     } else {
-      return new ErrorResponse('server',
+      return new ErrorResponse(errorTypes.server,
         'Ein interner Serverfehler ist aufgetreten. Bitte kontaktieren Sie Ihren Administrator.')
     }
   }
 
   createOfferNotFoundErrorResponse (token: string): ErrorResponse {
-    return new ErrorResponse('token', `Das Angebot mit Token '${token}' existiert nicht oder wurde gelöscht.`)
+    return new ErrorResponse(errorTypes.token, `Das Angebot mit Token '${token}' existiert nicht oder wurde gelöscht.`)
   }
 
   createOfferNotConfirmedErrorResponse (): ErrorResponse {
-    return new ErrorResponse('confirmation', 'Das Angebot wurde noch nicht bestätigt. Bitte bestätigen Sie Ihr Angebot zuerst.')
+    return new ErrorResponse(errorTypes.confirmation, 'Das Angebot wurde noch nicht bestätigt. Bitte bestätigen Sie Ihr Angebot zuerst.')
   }
 
   createOfferExpiredErrorResponse (token: string): ErrorResponse {
-    return new ErrorResponse('token', `Das Angebot mit Token '${token}' ist bereits abgelaufen.`)
+    return new ErrorResponse(errorTypes.token, `Das Angebot mit Token '${token}' ist bereits abgelaufen.`)
   }
 
   createValidationFailedErrorResponse (error: ValidationError): ErrorResponse {
     const fieldErrorMessages = Object.values(error.errors).map((e: mixed): string => e.message)
-    return new ErrorResponse('validation', `Im Formular sind die folgenden Fehler aufgetreten: ${fieldErrorMessages.join(' ')}`)
+    return new ErrorResponse(errorTypes.validation, `Im Formular sind die folgenden Fehler aufgetreten: ${fieldErrorMessages.join(' ')}`)
   }
 
   createValidationFailedErrorResponseFromArray (errors: Array<Error>): ErrorResponse {
     const errorFields = errors.array().map((error: Error): string => this.translateOuterFormPaths(error.param))
     const errorFieldsWithoutDuplicates = _.uniq(errorFields)
     const message = `Ungültige oder fehlende Eingaben in dem/den folgenden Feld(ern): ${errorFieldsWithoutDuplicates.join(', ')}`
-    return new ErrorResponse('validation', message)
+    return new ErrorResponse(errorTypes.validation, message)
   }
 
   translateOuterFormPaths (param: string): string {
