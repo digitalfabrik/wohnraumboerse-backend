a3e9cf57360449e2112cdcb443bd1b58b85bfe44
Nikolas Klug <klug.nikolas@googlemail.com>
2018-08-02 17:17:22 +0200

    NEUBURG-88: Add the specific cronjobs

--- src/cronjobs.js
+++ src/cronjobs.js
@@ -1,7 +1,7 @@
 // @flow
 
 import {CronJob} from 'cron'
-import Offer from '../models/Offer'
+import Offer from '/models/Offer'
 
 const TIMEZONE = 'Europe/Berlin'
 
@@ -16,10 +16,19 @@ const deleteExpired = async () => {
 }
 
 const deleteNotConfirmed = async () => {
+  const twoDaysAgo = Date.now()
+  twoDaysAgo.setDate(twoDaysAgo.getDate() - 2)
 
+  await Offer.delete()
+    .where('createdDate')
+    .lte(twoDaysAgo)
+    .where('confirmed')
+    .equals(false)
+    .exec()
 }
 
 export default () => {
+  // Execute at 00:00:00 every day
   const deleteExpiredJob = CronJob('00 00 00 * * *', deleteExpired, null, true, TIMEZONE)
 
   const deleteNotConfirmedJob = CronJob('00 00 00 * * *', deleteNotConfirmed, null, true, TIMEZONE)
