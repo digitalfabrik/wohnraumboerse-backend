ba1be1f0f8022f3f0429598a47a014c23f655483
Nikolas Klug <klug.nikolas@googlemail.com>
2018-08-03 16:14:20 +0200

    NEUBURG-50: Serve images statically

--- src/api/cityConfigs.js
+++ src/api/cityConfigs.js
@@ -1,7 +1,8 @@
 // @flow
 
 import type {$Request, $Response} from 'express'
-import {Router} from 'express'
+import express, {Router} from 'express'
+import path from 'path'
 import HttpStatus from 'http-status-codes'
 import cityConfigs from '../cities/cityConfigs'
 
@@ -11,6 +12,8 @@ export default (): Router => {
   router.get('/', (request: $Request, response: $Response) => {
     response.status(HttpStatus.OK).json(cityConfigs)
   })
+
+  router.use('/image', express.static(path.join(__dirname, '../cities/assets')))
 
   return router
 }
--- src/cities/cityConfigs.js
+++ src/cities/cityConfigs.js
@@ -2,27 +2,22 @@
 
 import CityConfig from './CityConfig'
 
-import logoNeuburg from './assets/neuburg_logo.svg'
-import logoBayreuth from './assets/bayreuth_logo.png'
-import faviconBayreuth from './assets/bayreuth_favicon.ico'
-import faviconNeuburg from './assets/neuburg_favicon.ico'
-
 const configs = {
   neuburgschrobenhausenwohnraum: new CityConfig({
     cmsName: 'neuburgschrobenhausenwohnraum',
     hostname: 'raumfrei.neuburg-schrobenhausen.de',
     formsEnabled: true,
     title: 'Raumfrei Neuburg-Schrobenhausen',
-    logo: logoNeuburg,
-    favicon: faviconNeuburg
+    logo: 'neuburg_logo.svg',
+    favicon: 'neuburg_favicon.ico'
   }),
   bayreuthwohnraum: new CityConfig({
     cmsName: 'bayreuthwohnraum',
     hostname: 'wohnraumboerse.bayreuth.de',
     formsEnabled: false,
     title: 'Bayreuth',
-    logo: logoBayreuth,
-    favicon: faviconBayreuth
+    logo: 'bayreuth_logo.png',
+    favicon: 'bayreuth_favicon.ico'
   })
 }
 
