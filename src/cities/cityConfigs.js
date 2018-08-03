4bc8a61cf4d9c4c0a6e9b6675f517bfdd6989ebe
Nikolas Klug <klug.nikolas@googlemail.com>
2018-08-03 11:56:46 +0200

    NEUBURG-26: Sychronize city configs with frontend

--- src/cities/cityConfigs.js
+++ src/cities/cityConfigs.js
@@ -2,20 +2,27 @@
 
 import CityConfig from './CityConfig'
 
+import logoNeuburg from './assets/neuburg_logo.svg'
+import logoBayreuth from './assets/bayreuth_logo.png'
+import faviconBayreuth from './assets/bayreuth_favicon.ico'
+import faviconNeuburg from './assets/neuburg_favicon.ico'
+
 const configs = {
   neuburgschrobenhausenwohnraum: new CityConfig({
     cmsName: 'neuburgschrobenhausenwohnraum',
     hostname: 'raumfrei.neuburg-schrobenhausen.de',
     formsEnabled: true,
     title: 'Raumfrei Neuburg-Schrobenhausen',
-    logo: null
+    logo: logoNeuburg,
+    favicon: faviconNeuburg
   }),
   bayreuthwohnraum: new CityConfig({
     cmsName: 'bayreuthwohnraum',
-    hostname: 'bayreuth.wohnen.integreat-app.de',
+    hostname: 'wohnraumboerse.bayreuth.de',
     formsEnabled: false,
     title: 'Bayreuth',
-    logo: null
+    logo: logoBayreuth,
+    favicon: faviconBayreuth
   })
 }
 
